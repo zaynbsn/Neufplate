db7664517fc7535caf51594228865445f5f7942b
zaynbsn <nils.vainui@gmail.com>
2023-10-20 15:48:13 +0200

    clean index

--- Index.php
+++ Index.php
@@ -9,17 +9,8 @@ $user = $builder
 ->addPhone("07 69 80 25 77")
 ->build();
 
-// echo($user->listUserInfos());
-// lineBreak();
-
 $neufplate = New NeufPlate();
 
 $provider = new Provider(ProviderType::DICEBEAR, SpriteType::lorelei);
 $nft = $neufplate->process($user, $provider);
-
-// lineBreak(2);
-
-// $robotProvider = new Provider(ProviderType::ROBOTHASH);
-// $neufplate->process($user, $robotProvider);
-
 ?>
